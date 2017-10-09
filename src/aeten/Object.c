5633a563a5719afa2e8e03ce611121ef0775d0ed
Thomas Pérennou <thomas.perennou@aeten.net>
2017-10-09 02:39:03 +0200

    Constructors returns parent interface instance

---
+++ src/aeten/Object.c
@@ -0,0 +1,20 @@
+#include <strings.h>
+
+#include "aeten/Object.h"
+
+
+//! \memberof aeten__Object
+void aeten__Object__delete(aeten__Object* self) {
+	// TODO: Implement
+}
+
+//! \memberof aeten__Object
+aeten__Object* aeten__Object__clone(aeten__Object* self) {
+	// TODO: Implement
+	return NULL;
+}
+
+//! \memberof aeten__Object
+void aeten__Object__copy(aeten__Object* self, aeten__Object* dest) {
+	// TODO: Implement
+}
