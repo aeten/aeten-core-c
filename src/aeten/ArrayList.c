6cd7406ce50ee7820533da3324cb8c2bab7a6be3
Thomas Pérennou <thomas.perennou@aeten.net>
2017-10-26 18:20:06 +0200

    Implementation of ArrayList.removeAt() operation

--- src/aeten/ArrayList.c
+++ src/aeten/ArrayList.c
@@ -18,6 +18,9 @@ namespace aeten {
 		- elements : void*
 
 		{static} + ArrayList(size_t initial_capacity, size_t element_size) <<constructor>>
+		bool add(T* element) <<override>>
+		removeAt(size_t position) <<override>>
+		set(size_t position, T* element) <<override>>
 		# finalize() <<override>>
 	}
 	note right of ArrayList::ArrayList
@@ -51,7 +54,22 @@ void _set(ArrayList *self, size_t position, void *element) {
 	memcpy(((char*)self->_elements) + (position * self->_element_size), element, self->_element_size);
 }
 
-void _add(ArrayList *self, void *element) {
+void _removeAt(ArrayList *self, size_t position) {
+	check(position < self->_size, IndexOutOfBoundException, "position=%u; array.length=%u", position, self->_size);
+	void *dst = ((char*)self->_elements) + (position * self->_element_size);
+	if (position < (self->_size-1)) {
+		void *src = ((char*)self->_elements) + ((1 + position) * self->_element_size);
+		size_t size = (self->_size - position) * self->_element_size;
+		memmove(dst, src, size);
+		memset(((char*)self->_elements) + ((self->_size - 2) * self->_element_size), 0x0, self->_element_size);
+	} else {
+		memset(dst, 0x0, self->_element_size);
+	}
+	--self->_size;
+}
+
+
+bool _add(ArrayList *self, void *element) {
 	if (self->_capacity == self->_size) {
 		unsigned int capacity = ((self->_capacity * 3) / 2) + 1;
 		self->_elements = realloc(self->_elements, capacity * self->_element_size);
@@ -59,6 +77,7 @@ void _add(ArrayList *self, void *element) {
 		self->_capacity = capacity;
 	}
 	memcpy(((char*)self->_elements) + (self->_size++ * self->_element_size), element, self->_element_size);
+	return true;
 }
 
 void *_get(ArrayList *self, size_t position) {
