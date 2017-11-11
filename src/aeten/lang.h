4202ae325e576706a7480d1c10e7f580ece8b97d
Thomas Pérennou <thomas.perennou@aeten.net>
2017-11-11 21:43:06 +0100

    Add throw macro and allowing to not give message in for check

--- src/aeten/lang.h
+++ src/aeten/lang.h
@@ -19,13 +19,30 @@ typedef struct {
 //! }
 //! @enduml
 
+#define _HAS_FMT_(...) _HAS_FMT1_(X, ##__VA_ARGS__)
+#define _HAS_FMT1_(...) _HAS_FMT2_(__VA_ARGS__, 1, 0)
+#define _HAS_FMT2_(a0, a1, N, ...) N
+#define _SEMICOLON__0
+#define _SEMICOLON__1 ": "
+#define _SEMICOLON__x_(x) _SEMICOLON__##x
+#define _SEMICOLON__x(x) _SEMICOLON__x_(x)
+#define _SEMICOLON_(...) _SEMICOLON__x(_HAS_FMT_(__VA_ARGS__))
+#define _FMT_(fmt, ...) _SEMICOLON_(fmt) fmt
+#define _MSG_ATTR_(msg, ...) , ##__VA_ARGS__
+#define _MSG_FORMAT_(...) _FMT_(__VA_ARGS__) "\n" _MSG_ATTR_(__VA_ARGS__)
+
 /** Check expression or throws exception (TODO) */
-#define check(expression, exception, message_format, ...) { \
+#define check(expression, exception, ...) do { \
 		if (!(expression)) { \
-			fprintf(stderr, #exception ": " message_format "\n", ##__VA_ARGS__); \
+			fprintf(stderr, #exception _MSG_FORMAT_(__VA_ARGS__)); \
 			assert(expression); \
 		} \
-	}
+	} while (0);
+
+/** Throws exception (TODO) */
+#define throw(exception, ...) do { \
+		fprintf(stderr, #exception _MSG_FORMAT_(__VA_ARGS__)); \
+	} while (0);
 
 #define _AETEN_MARK 0x01C3860A /* SOH Æ 10 */
 #define isObject(pointer) ( \
