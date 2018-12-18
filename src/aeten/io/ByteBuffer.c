cb3b8f26276ff930c90318861981eae508f8bd84
Thomas Pérennou <thomas.perennou@aeten.net>
2018-12-19 00:41:18 +0100

    Continue implementation of direct byte buffer

--- src/aeten/io/ByteBuffer.c
+++ src/aeten/io/ByteBuffer.c
@@ -13,6 +13,14 @@ namespace aeten.io {
 		uint8_t getByte(size_t index)
 		put(uint8_t* src, size_t offset, size_t length)
 		putByte(uint8_t b)
+		ByteBuffer slice()
+		size_t position()
+		size_t capacity()
+		size_t limit()
+		setLimit(size_t newLimit)
+		setPosition(size_t newPosition)
+		bool hasArray()
+		uint8_t* array()
 	}
 }
 @enduml
@@ -26,6 +34,7 @@ namespace aeten.io {
 
 		- buffer: uint8_t*
 		- capacity: size_t
+		- limit: size_t
 		- position: size_t
 		- mark: size_t
 	}
@@ -45,14 +54,16 @@ void DirectByteBuffer_new(DirectByteBuffer *self, size_t capacity) {
 	self->_capacity = capacity;
 	self->_buffer = (uint8_t*)calloc(capacity, sizeof(uint8_t));
 	check(self->_buffer, HeapAllocationException, "%m (%u)", errno);
+	self->_limit = 0;
 	self->_position = 0;
 }
 
 void DirectByteBuffer_new_from(DirectByteBuffer *self, uint8_t* src, size_t offset, size_t length) {
-	self->_buffer = src;
+	self->_buffer = src + offset;
 	check(self->_buffer, NullPointerException);
 	self->_capacity = length;
-	self->_position = offset;
+	self->_limit = length;
+	self->_position = 0;
 }
 
 size_t DirectByteBuffer_capacity(DirectByteBuffer *self) {
@@ -80,7 +91,23 @@ size_t DirectByteBuffer_position(DirectByteBuffer *self) {
 	return self->_position;
 }
 
+size_t DirectByteBuffer_limit(DirectByteBuffer *self) {
+	return self->_limit;
+}
+
+void DirectByteBuffer_setLimit(DirectByteBuffer *self, size_t newLimit) {
+	check(newLimit <= self->_capacity, IndexOutOfBoundException, "new limit %zu over buffer capacity (%zu)", newLimit, self->_capacity);
+	if (self->_position > newLimit) {
+		self->_position = newLimit;
+	}
+	if (self->_mark > newLimit) {
+		self->_mark = 0;
+	}
+	self->_limit = newLimit;
+}
+
 void DirectByteBuffer_setPosition(DirectByteBuffer *self, size_t newPosition) {
+	check(newPosition <= self->_limit, IndexOutOfBoundException, "new position %zu over buffer limit (%zu)", newPosition, self->_limit);
 	self->_position = newPosition;
 	if (self->_mark > newPosition) {
 		self->_mark = 0;
@@ -96,19 +123,35 @@ void DirectByteBuffer_rewind(DirectByteBuffer *self) {
 	self->_mark = 0;
 }
 
+bool DirectByteBuffer_hasArray(DirectByteBuffer *self) {
+	return true;
+}
+
+uint8_t* DirectByteBuffer_array(DirectByteBuffer *self) {
+	return self->_buffer;
+}
+
 uint8_t DirectByteBuffer_getByte(DirectByteBuffer *self, size_t index) {
+	check(index > self->_limit, IndexOutOfBoundException, "index %zu over buffer limit (%zu)", index, self->_limit);
 	return self->_buffer[index];
 }
 
 void DirectByteBuffer_putByte(DirectByteBuffer *self, uint8_t b) {
+	check(self->_position < self->_limit, BufferOverflowException, "Limit has joined buffer capacity (%zu)", self->_limit);
 	*(self->_buffer + self->_position++) = b;
 }
 
 void DirectByteBuffer_get(DirectByteBuffer *self, uint8_t* dst, size_t offset, size_t length) {
+	check(length > (self->_limit - self->_position), BufferUnderflowException, "Attempt to get %zu bytes while %zu available", length, self->_limit - self->_position);
 	memcpy(dst + offset, self->_buffer + self->_position, length);
 }
 
+ByteBuffer DirectByteBuffer_slice(DirectByteBuffer *self) {
+	return ByteBuffer_wrap(self->_buffer, self->_position, self->_limit - self->_position);
+}
+
 void DirectByteBuffer_put(DirectByteBuffer *self, uint8_t* src, size_t offset, size_t length) {
+	check((self->_position + length) > self->_capacity, BufferOverflowException, "Attempt to put %zu bytes while remaining %zu", length, self->_capacity - self->_limit);
 	memcpy(self->_buffer + self->_position, src + offset, length);
 	self->_position += length;
 }
